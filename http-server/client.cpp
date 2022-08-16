c11785e69a1827bc48164d9d11b850581333a4e9
Olcay <34381831+olcayarpac@users.noreply.github.com>
2022-08-16 22:04:42 +0300

    client added

--- http-server/client.cpp
+++ http-server/client.cpp
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
